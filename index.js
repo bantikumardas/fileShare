3e41b4bfb4ec2b9f046aedaba9694591c255018a
bantikumardas <bantikumardas43@gmail.com>
2023-05-13 23:49:49 +0530

    push

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ app.use(express.static('public'));
 
 
 //routes
-app.use('/', (req, res)=>{
-    res.json({message:"hello this is banti"});
-});
+
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-
+app.use('/', (req, res)=>{
+    res.json({message:"hello this is banti"});
+});
 app.listen(PORT, ()=>{
     console.log(`server is running at port no ${PORT}`);
 })

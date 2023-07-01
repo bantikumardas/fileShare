74821dc992e4dbdfd002375c06d19dec8deaf231
bantikumardas <bantikumardas43@gmail.com>
2023-07-02 00:34:40 +0530

    fix time of unlink

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const fs = require('fs');
 
 async function fetchData() {
     // 24 hours 
-    const pastDate = new Date(Date.now() - 1000 * 60*1);
+    const pastDate = new Date(Date.now() - 1000 * 60*60*24);
     console.log("called");
     const files =await File.find({ createdAt: { $lt: pastDate } });
     console.log(files.length);
     if (files.length) {
         for (const file of files) {
             try {
-                fs.unlinkSync(file.path);
                 await file.deleteOne({uuid:file.uuid});
+                fs.unlinkSync(file.path);
                 console.log(`successfully deleted ${file.filename}`);
             }catch(err){
-                console.log(`Error while deleting files ${err}`)
+                // console.log(`Error while deleting files ${err}`)
             }
            
         }
